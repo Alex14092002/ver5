b5222101af176c6afd2b8b8562d94c67b1eb2b8c
DEV PHU TUAN <2051010343tuan@ou.edu.vn>
2023-04-01 18:30:28 +0700

    update

--- number.js
+++ number.js
@@ -1,25 +1,35 @@
+// let amountElement = document.getElementById('amount');
+// let amount = parseInt(amountElement.value);
 
- let amountElement = document.getElementById('amount')
+// let render = (amount) => {
+//   amountElement.value = amount;
+ 
+// };
 
-  let amount = amountElement.value
+
+// let handlePlus = () => {
+//   amount++;
+//   render(amount);
+// };
+// let handleMinus = () => {
+//   if (amount > 1) {
+//     amount--;
+//     render(amount);
+//   }
+// }
 
 
+function increaseValue() {
+  var value = parseInt(document.getElementById('amount').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  document.getElementById('amount').value = value;
+}
 
-  let render = (amount) =>{
-     amountElement.value = amount;
-  }
-
-  let handlePlus = () =>{
-    
-    amount ++
-   
-    render(amount)
-  }
-  let handleMinus = () =>{
-    if(amount > 1){
-      amount --
-   
-    render(amount)
-    }
-    
-  }
+function decreaseValue() {
+  var value = parseInt(document.getElementById('amount').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value < 1 ? value = 1 : '';
+  value--;
+  document.getElementById('amount').value = value;
+}
--- number2.js
+++ number2.js
@@ -1,26 +1,41 @@
 
-let amountElement2 = document.getElementById('amount2')
+// let amountElement2 = document.getElementById('amount2')
 
 
-let amount2 = amountElement2.value
+// let amount2 = amountElement2.value
 
 
 
-let render2 = (amount2) =>{
-   amountElement2.value = amount2;
+// let render2 = (amount2) =>{
+//    amountElement2.value = amount2;
+// }
+
+// let handlePlus2 = () =>{
+  
+//   amount2 ++
+ 
+//   render2(amount2)
+// }
+// let handleMinus2 = () =>{
+//   if(amount2 > 1){
+//     amount2 --
+ 
+//   render2(amount2)
+//   }
+  
+// }
+
+function increaseValue2() {
+  var value = parseInt(document.getElementById('amount2').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  document.getElementById('amount2').value = value;
 }
 
-let handlePlus2 = () =>{
-  
-  amount2 ++
- 
-  render2(amount2)
-}
-let handleMinus2 = () =>{
-  if(amount2 > 1){
-    amount2 --
- 
-  render2(amount2)
-  }
-  
+function decreaseValue2() {
+  var value = parseInt(document.getElementById('amount2').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value < 1 ? value = 1 : '';
+  value--;
+  document.getElementById('amount2').value = value;
 }
